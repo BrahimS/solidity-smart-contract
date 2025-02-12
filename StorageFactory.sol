d4f3f2c599af6d8efec4e82873e3c376f87eb8e2
brahims <bsourny@gmail.com>
2025-02-12 10:53:49 +0100

    feat(functions): interact with contracts ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,8 +5,34 @@ pragma solidity >=0.7.0 <0.9.0;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-  SimpleStorage public simpleStorage;
-  function createSimpleStorageContract() public {
-    simpleStorage = new SimpleStorage();
-  }
+    // SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        // simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function SimpleForgeStore(
+        uint256 _simpleStorageIndex,
+        uint256 _newSimpleStorageNumber
+    ) public {
+        // In order to interfere with a contract, i will need two things:
+        // 1 - Adress
+        // 2 - ABI - Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[
+            _simpleStorageIndex
+        ];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function getSimpleForege(
+        uint256 _simpleStorageIndex
+    ) public view returns (uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[
+            _simpleStorageIndex
+        ];
+        return mySimpleStorage.getFavorite256Num();
+    }
 }
