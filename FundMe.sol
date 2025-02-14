790d21bd3078ba62f2add2be3b55d936ae24f8eb
Sourny <bsourny@gmail.com>
2025-02-14 15:41:30 +0100

    feat(functions): reseting an array

--- FundMe.sol
+++ FundMe.sol
@@ -39,6 +39,8 @@ contract FundMe {
               address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
           }
+          // Rest the address Array
+        funders = new address[](0);
     }
 
 }
