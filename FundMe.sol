b0dc4b6004d6bdcf0e9352c4343512163920de5d
brahims <bsourny@gmail.com>
2025-02-14 10:52:12 +0100

    feat(functions): Importing libaries from NPM and Github

--- FundMe.sol
+++ FundMe.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     /*
     Tasks:
@@ -9,11 +11,32 @@ contract FundMe {
     2 - Withdraw funds
     3 - Set a minimum funding value un USD
     */
+    uint256 public minimumUsd = 2e18;
 
     function fund() public payable {
         // Allowed users to send $
         // Have a minimum $ sent
         //// 1 - How do we send ETH to this contract
-        require(msg.value > 1e18, "You can't send less than 1 Ether");
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "You can't send less than 1 Ether"
+        );
+    }
+
+    function getPrice() public view returns (uint256) {
+        // address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e18);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
